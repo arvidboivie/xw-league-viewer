c307f3704e693168917d4437e5c2de266977f4c9
Arvid Boivie <arvid.boivie@gmail.com>
2018-01-07 09:55:02 +0100

    Twig debug

--- public/index.php
+++ public/index.php
@@ -21,11 +21,13 @@ $container['logger'] = function ($c) {
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig('../templates', [
         'cache' => '../cache',
+        'debug' => true,
     ]);
 
     // Instantiate and add Slim specific extension
     $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
     $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+    $view->addExtension(new \Twig_Extension_Debug());
 
     return $view;
 };
